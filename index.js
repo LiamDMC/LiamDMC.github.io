f1a334e873461b1550848478441e2987b8937df9
LiamDMC <60507779+LiamDMC@users.noreply.github.com>
2021-04-15 17:40:38 +0100

    Updates

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ var i = 0;
 var txt = 'HTML,CSS,JavaScript,Jquery,Node.JS,React.JS, PHP,SQL';
 var speed = 75;
 
+
+
+
+
  window.onload = function typeWriter() {
   if (i < txt.length) {
     document.getElementById("Skills").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
+
 
 };
